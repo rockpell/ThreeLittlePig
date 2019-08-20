6917d77b81f33583b4dafe60fbade55de6555112
rockpell <aythffk@gmail.com>
2019-08-20 23:30:01 +0900

    UIManager 클래스 추가 및 프리팹 생성 현재 벽 타입 버튼 기능 연결

---
+++ WolfRun/Assets/Scripts/UIManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] wallButtons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void selectWallTypeButton(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                GameManager.Instance.Player.NowConstructWallType = WallType.STRAW;
+                break;
+            case 1:
+                GameManager.Instance.Player.NowConstructWallType = WallType.WOOD;
+                break;
+            case 2:
+                GameManager.Instance.Player.NowConstructWallType = WallType.BRICK;
+                break;
+        }
+        highlightButton(index);
+    }
+
+    private void highlightButton(int index)
+    {
+
+    }
+}
