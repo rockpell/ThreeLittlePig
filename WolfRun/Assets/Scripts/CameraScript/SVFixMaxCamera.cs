9d066705739b2eed37e42e8f12e55f0b038cdcbd
rockpell <aythffk@gmail.com>
2019-08-22 01:56:12 +0900

    카메라 이동 영역 제한 스크립트 추가

---
+++ WolfRun/Assets/Scripts/CameraScript/SVFixMaxCamera.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SVFixMaxCamera : MonoBehaviour
+{
+    public bool follow = true; //Whether the camera should follow the player
+    private Vector3 moveTo; //Target vector
+    private Pig player;
+
+    [SerializeField] private GameObject bottomBorder = null; 
+    [SerializeField] private GameObject upBorder = null; 
+    [SerializeField] private GameObject leftBorder = null;
+    [SerializeField] private GameObject rightBorder = null;
+
+    private float xMin;
+    private float xMax;
+    private float yMin;
+    private float yMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Pig>();
+        InitBorder();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Mathf.Clamp(player.transform.position.x, xMin, xMax);
+        float y = Mathf.Clamp(player.transform.position.y, yMin, yMax);
+        transform.position = new Vector3(x, y, transform.position.z);
+    }
+
+    private void InitBorder()
+    {
+        xMin = leftBorder.transform.position.x;
+        xMax = rightBorder.transform.position.x;
+        yMin = bottomBorder.transform.position.y;
+        yMax = upBorder.transform.position.y;
+    }
+}
