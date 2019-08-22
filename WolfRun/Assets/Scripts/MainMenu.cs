55885ecdab11985fb1e09fd84334a3e94b0e9de6
rockpell <aythffk@gmail.com>
2019-08-23 00:04:07 +0900

    메인 메뉴 버튼 기능 작동하도록 수정

---
+++ WolfRun/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void gameStart()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+    }
+
+    public void gameExit()
+    {
+        Application.Quit(0);
+    }
+}
